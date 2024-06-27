b951828bca14f844751ab9bacd94d897ffc85a45
pep-per <98595503+pep-per@users.noreply.github.com>
2024-06-27 20:49:43 +0900

    [level 1] Title: 소수 만들기, Time: 0.04 ms, Memory: 4.14 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.cpp
@@ -0,0 +1,41 @@
+#include <vector>
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int count = 0;
+
+bool isPrime(int num) {
+    int x = 2;
+    while (x<=num/sqrt(2)) {
+        if (num%x == 0)
+            return false;
+        else
+            x++;
+    }
+    return true;
+}
+
+void combination(int idx, vector<int> nums, vector<int> comb) {
+    if (comb.size() == 3) {
+        int sum = 0;
+        for (auto x : comb)
+            sum += x;
+        if (isPrime(sum))
+            count++;
+        return;
+    }
+    for (int i = idx+1; i < nums.size(); i++) {
+        cout << nums[i] << endl;
+        comb.push_back(nums[i]);
+        combination(i, nums, comb);
+        comb.pop_back();
+    }
+}
+
+int solution(vector<int> nums) {
+    vector<int> comb;
+    combination(-1, nums, comb);
+    return count;
+}
