8eb99a87cb25152fffb20304aafb3ba14150200f
pep-per <98595503+pep-per@users.noreply.github.com>
2024-07-18 22:07:24 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int n;
+  cin >> n;
+  vector<long long> dp(n + 1, 0);
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for (int i = 2; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2] * 2) % 10007;
+  }
+  cout << dp[n] << endl;
+  return 0;
+}
