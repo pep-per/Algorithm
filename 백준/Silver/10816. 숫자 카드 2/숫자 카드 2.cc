5b1b9d97c56c2b7dee713529a90dca75cfb348d1
pep-per <98595503+pep-per@users.noreply.github.com>
2024-07-30 19:07:30 +0900

    Update 숫자 카드 2.cc

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -1,27 +1,9 @@
 #include <iostream>
+#include <map>
 #include <vector>
 #include <algorithm>
 
 using namespace std;
-
-// 이진 탐색 함수
-int binarySearch(const vector<pair<int, int>>& arr, int target) {
-    int left = 0, right = arr.size() - 1;
-    
-    while (left <= right) {
-        int mid = left + (right - left) / 2;
-        if (arr[mid].first == target) {
-            return arr[mid].second;
-        }
-        if (arr[mid].first < target) {
-            left = mid + 1;
-        } else {
-            right = mid - 1;
-        }
-    }
-    
-    return 0;
-}
 
 int main() {
     ios_base::sync_with_stdio(false);
@@ -29,37 +11,28 @@ int main() {
     
     int n, num;
     cin >> n;
-    vector<int> numbers;
+    map<int, int> freq;
     
-    // 입력 받기
     for (int i = 0; i < n; i++) {
         cin >> num;
-        numbers.push_back(num);
+        freq[num]++;
     }
     
-    // 정렬
-    sort(numbers.begin(), numbers.end());
-    
-    // 빈도수 계산
-    vector<pair<int, int>> freq;
-    int count = 1;
-    for (int i = 1; i < n; i++) {
-        if (numbers[i] == numbers[i-1]) {
-            count++;
-        } else {
-            freq.push_back({numbers[i-1], count});
-            count = 1;
-        }
+    vector<int> keys;
+    for (const auto& pair : freq) {
+        keys.push_back(pair.first);
     }
-    freq.push_back({numbers[n-1], count});
+    sort(keys.begin(), keys.end());
     
-    // 쿼리 처리
     int m;
     cin >> m;
-    for (int i = 0; i < m; i++){
+    for (int i = 0; i < m; i++) {
         cin >> num;
-        cout << binarySearch(freq, num) << " ";
+        if (binary_search(keys.begin(), keys.end(), num)) {
+            cout << freq[num] << ' ';
+        } else {
+            cout << "0 ";
+        }
     }
-    
     return 0;
 }
