f3c04c96c1149f44e570d0f8f111756935934ede
pep-per <98595503+pep-per@users.noreply.github.com>
2025-01-14 09:42:09 +0900

    [Gold V] Title: LCS, Time: 4 ms, Memory: 5984 KB -BaekjoonHub

---
+++ 백준/Gold/9251. LCS/LCS.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+  string str1, str2;
+  cin >> str1 >> str2;
+
+  int m = str1.length();
+  int n = str2.length();
+  vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+
+  for (int i = 1; i <= m; i++) {
+    for (int j = 1; j <= n; j++) {
+      if (str1[i - 1] == str2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1;
+      } else {
+        dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+      }
+    }
+  }
+
+  cout << dp[m][n] << endl;
+  return 0;
+}
