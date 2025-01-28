0604e2f792f134e3971e5ea0a9eb0c6d9cf6da0c
pep-per <98595503+pep-per@users.noreply.github.com>
2025-01-29 00:04:56 +0900

    [level 2] Title: 업그레이드 된 아이템 구하기, Time: , Memory: undefined -BaekjoonHub

---
+++ 프로그래머스/2/273711. 업그레이드 된 아이템 구하기/업그레이드 된 아이템 구하기.sql
@@ -0,0 +1,9 @@
+SELECT info.ITEM_ID, info.ITEM_NAME, info.RARITY
+FROM ITEM_INFO AS info
+LEFT JOIN ITEM_TREE AS tree
+ON info.ITEM_ID = tree.ITEM_ID
+WHERE tree.PARENT_ITEM_ID IN (SELECT ITEM_ID
+                             FROM ITEM_INFO
+                             WHERE RARITY = 'RARE')
+AND tree.PARENT_ITEM_ID IS NOT NULL
+ORDER BY info.ITEM_ID DESC;
