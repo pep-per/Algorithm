1138786e8265f28ec1ecc42b7cc28cd25e02a66e
pep-per <98595503+pep-per@users.noreply.github.com>
2025-02-13 21:33:52 +0900

    [level 2] Title: 행렬의 곱셈, Time: 2.02 ms, Memory: 4.88 MB -BaekjoonHub

---
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+vector<vector<int>> solution(vector<vector<int>> arr1, vector<vector<int>> arr2) {
+    vector<vector<int>> answer;
+    
+    for (int i = 0; i < arr1.size(); i++) {
+        vector<int> tmp;
+        for (int j = 0; j < arr2[0].size(); j++) {
+            int sum = 0;
+            for (int k = 0; k < arr1[i].size(); k++) {
+                sum += arr1[i][k] * arr2[k][j];
+            }
+            tmp.push_back(sum);
+        }
+        answer.push_back(tmp);
+    }
+    return answer;
+}
